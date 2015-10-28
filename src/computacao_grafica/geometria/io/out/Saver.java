066630fcd78073c84a485471c33f530d25671325
felruivo <felipe.piacsek@fdte.org.br>
2015-10-28 18:44:53 -0200

    XML bonitinho.

--- src/computacao_grafica/geometria/io/out/Saver.java
+++ src/computacao_grafica/geometria/io/out/Saver.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -32,6 +33,8 @@ public class Saver {
         try {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
             DOMSource source = new DOMSource(documento);
             StreamResult result = new StreamResult(new File(filename));
             transformer.transform(source, result);
